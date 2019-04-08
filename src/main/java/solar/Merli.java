230449e6b6b399e73199df9fb1925ff0226cfc4a
Alexander <alexandergorokhov07@gmail.com>
2019-04-08 19:24:42 -0300

    Cloud deployment

--- src/main/java/solar/Merli.java
+++ src/main/java/solar/Merli.java
@@ -1,13 +1,19 @@
 package solar;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class Merli {
+  private static Logger logger = LoggerFactory.getLogger(Merli.class);
+
 
     public static void main(String[] args) {
-
+        logger.info("Starting Merli");
         SpringApplication.run(Merli.class, args);
     }
 }
